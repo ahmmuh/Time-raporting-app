01e7b13b321550c5a9c71e3a4b91cd8c59c8b225
ahmmuh <mukhtar1100@hotmail.com>
2021-03-16 17:05:28 +0100

    ok

---
+++ js/app.js
@@ -0,0 +1,65 @@
+const getData = () => {
+  fetch("../data/project.json")
+    .then((response) => response.json())
+    .then((data) => showData(data));
+};
+
+getData();
+const showData = (data) => {
+  let selectElements = document.querySelector("#project");
+  data.forEach((project) => {
+    let option = document.createElement("option");
+    option.innerHTML = project.title;
+    selectElements.appendChild(option);
+    selectedProject(option);
+  });
+};
+
+const selectedProject = (option) => {
+  //   console.log(option);
+};
+
+const getActivity = () => {
+  fetch("../data/activity.json")
+    .then((res) => res.json())
+    .then((data) => showActivity(data));
+};
+
+getActivity();
+const showActivity = (data) => {
+  const activities = document.getElementById("activity");
+  data.forEach((act) => {
+    const activity = document.createElement("option");
+    activity.innerHTML = act.title;
+    activities.appendChild(activity);
+  });
+};
+
+// save hours
+// and date
+// save selected project
+// save selected activity
+let hours = [{}];
+const hourInput = document.querySelector("#time").value;
+let datepicker = document.querySelector("#datepicker").value;
+console.log(hourInput);
+console.log(datepicker);
+// let date = new Date();
+// time = date.toLocaleString();
+// raport btn
+const raport = document.querySelector("#raport-btn");
+raport.addEventListener("click", function () {
+  hours.push(hourInput.value, time);
+  // if (hourInput.value >= 0.1 && hourInput.value <= 1) {
+  //   localStorage.setItem("key", hours);
+  //   console.log(time);
+  // } else {
+  //   alert("Ditt värde måste vara mellan 0 och 1");
+  // }
+});
+
+const raports = () => {
+  let allProjects = localStorage.getItem("key");
+};
+
+raports();
---
+++ js/date.js
@@ -0,0 +1,4 @@
+$(function () {
+  var date = $("#datepicker");
+  date.datepicker();
+});
